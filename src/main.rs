b877ef27f7a1299d6a6fb40b7130cadabe1c8c4c
soloworldsolo <shaminsolomonit@gmail.com>
2023-09-20 11:11:24 +0530

    encryption decryption completed

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,59 @@
-use std::fs;
-use std::path::Path;
-use pbkdf2::Pbkdf2;
-use sha2::Sha256;
+extern crate aes_gcm;
+extern crate rand;
+
+use std::io::{Error, ErrorKind};
+use aes_gcm::{
+    aead::{Aead, AeadCore, KeyInit, OsRng,generic_array::GenericArray,Payload},
+    Aes256Gcm, Key, Nonce
+};
+
+use rand::Rng;
 
 
+pub struct block {
+    data: Vec<u8>,
+    nonce: Vec<u8>
+}
 fn main() {
-    println!("Hello, world!");
-    let path = Path::new("/Users/hokage/Documents/edited.jpeg");
-    let vecl = fs::read(&path).expect("could not re");
-    let  key = b"key-generatorreanddddom";
-    let mut buf = [0u8; 20];
-    let  vec_arr = vecl.as_slice();
-    let salt = b"salsalesalasale";
-    println!("{:?}",vec_arr);
-    let mut key1 = [0u8; 20];
+   let enc = encrypt("soloworld".as_bytes(), "5zOYqRvMSbbNPRfFtct3fYogQszzucF7");
+}
 
-    pbkdf2::pbkdf2_hmac::<Sha256>(vec_arr,salt,600_000,&mut key1);
 
-    println!("{:?}", vec_arr)
+pub fn decrypt( encryptedTex: &block,  password:&str) {
+    let password_byte = password.as_bytes();
+    let key: &Key<Aes256Gcm> = password_byte.into();
+    let nonce = &encryptedTex.nonce;
+    let data = &encryptedTex.data;
+
+   let nonce = aes_gcm::Nonce::from_slice(&nonce);
+
+
+    let cipher = Aes256Gcm::new(&key);
+    let op = cipher.decrypt(&nonce ,data.as_slice());
+    //  let plaintext = cipher.decrypt(nonce, &*encryptedTex[15..].as_ref()).unwrap();
+}
+pub fn encrypt(data: &[u8], password: &str)  {
+    let password_byte = password.as_bytes();
+
+    let key: &Key<Aes256Gcm> = password_byte.into();
+    let cipher = Aes256Gcm::new(&key);
+    let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
+
+
+    let encrypted_data = match cipher.encrypt(&nonce,data)
+    {
+        Ok(encrpted) => {
+
+            let e = block { data: encrpted, nonce:nonce.to_vec() };
+            e
+        }
+        Err(err) => {
+            panic!("could not encrypt")
+        }
+    };
+
+
+decrypt(&encrypted_data ,password);
+
+
 }
